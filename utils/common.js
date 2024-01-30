b0b31c6776febe101babfa7426317ae4e2082e1a
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2024-01-30 17:19:38 +0600

    add prettierrc

--- utils/common.js
+++ utils/common.js
@@ -4,36 +4,36 @@ import fs from 'fs';
 
 export class commonFunctions{
 
-randomEmail = () => {
-    const domains = ['gmail.com', 'yahoo.com', 'outlook.com']; // Add more domains if needed
-    const randomString = Math.random().toString(36).substring(7);
-    const randomDomain = domains[Math.floor(Math.random() * domains.length)]; 
-    const email = `${randomString}@${randomDomain}`;
-    return email;
-  }
+    randomEmail = () => {
+        const domains = ['gmail.com', 'yahoo.com', 'outlook.com']; // Add more domains if needed
+        const randomString = Math.random().toString(36).substring(7);
+        const randomDomain = domains[Math.floor(Math.random() * domains.length)]; 
+        const email = `${randomString}@${randomDomain}`;
+        return email;
+    }
 
-randomPassword = () => {
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?";
-    let password = "";
-    for (let i = 0; i < 10; i++) {
-      const randomIndex = Math.floor(Math.random() * charset.length);
-      password += charset.charAt(randomIndex);
-    } 
-    return password;
-  }
+    randomPassword = () => {
+        const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?";
+        let password = "";
+        for (let i = 0; i < 10; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset.charAt(randomIndex);
+        } 
+        return password;
+    }
 
-  saveCredentialsToJson = (email, password) => {
-    const credentials = {
-      email,
-      password,
+    saveCredentialsToJson = (email, password) => {
+        const credentials = {
+        email,
+        password,
+        };
+
+        const jsonString = JSON.stringify(credentials, null, 2);
+
+        // Use path.join to ensure cross-platform compatibility
+        const filePath = path.join(__dirname, '../data/registerUser.json');
+    
+        fs.writeFileSync(filePath, jsonString, 'utf-8');
     };
-
-    const jsonString = JSON.stringify(credentials, null, 2);
-
-    // Use path.join to ensure cross-platform compatibility
-  const filePath = path.join(__dirname, '../data/registerUser.json');
-  
-  fs.writeFileSync(filePath, jsonString, 'utf-8');
-  };
 
 }
